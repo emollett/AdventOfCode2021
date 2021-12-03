b42b4a6ecb409327e052cc2b29cf546b977064c6
Eleanor Mollett <eleanor.mollett@gmail.com>
2021-12-03 19:31:14 +0000

    Day 3 part 2 WIP Test cases working real data not yay

--- 03/puzzle.py
+++ 03/puzzle.py
@@ -1,12 +1,14 @@
 from collections import Counter
+from typing import ItemsView
 
-def parse(lines):
-   input = [] 
-   for line in lines:
-      input.append(list(line.strip()))
-   
+def basicParse(input):
+   output = [] 
+   for line in input:
+      output.append(list(line.strip()))
+   return output
+
+def rotateInput(input):
    rotated_input = list(zip(*input))
-
    data = []
    for line in rotated_input:
       data.append("".join(line))
@@ -14,11 +16,11 @@ def parse(lines):
    return data
 
 def solve(data):
-
+   rotatedData = rotateInput(data)
    gamma = ''
    epsilon = ''
 
-   for line in data:
+   for line in rotatedData:
       res = Counter(line)
       if res["1"] > len(line)/2:
          gamma = gamma + '1'
@@ -28,3 +30,42 @@ def solve(data):
          epsilon = epsilon + '1'
 
    return int(gamma,2) * int(epsilon,2)
+
+def calculateOxygenRating(input):
+   originalInput = basicParse(input)
+   rotatedInput = rotateInput(originalInput)
+
+   indexOfBit = 0
+
+   while len(originalInput) > 1:
+      res = Counter(rotatedInput[indexOfBit])
+      if res["1"] >= len(originalInput)/2:
+         originalInput = [i for i in originalInput if i[indexOfBit] == '1']
+         rotatedInput = rotateInput(originalInput)
+      else:
+         originalInput = [i for i in originalInput if i[indexOfBit] == '0']
+         rotatedInput = rotateInput(originalInput)
+      indexOfBit += 1
+
+   return int("".join(originalInput[0]),2)
+
+def calculateCO2Rating(input):
+   originalInput = basicParse(input)
+   rotatedInput = rotateInput(originalInput)
+
+   indexOfBit = 0
+
+   while len(originalInput) > 1:
+      res = Counter(rotatedInput[indexOfBit])
+      if res["1"] >= len(originalInput)/2:
+         originalInput = [i for i in originalInput if i[indexOfBit] == '0']
+         rotatedInput = rotateInput(originalInput)
+      else:
+         originalInput = [i for i in originalInput if i[indexOfBit] == '1']
+         rotatedInput = rotateInput(originalInput)
+      indexOfBit += 1
+
+   return int("".join(originalInput[0]),2)
+
+def solve2(data):
+   return calculateCO2Rating(data) * calculateOxygenRating(data)
--- 03/test_puzzle.py
+++ 03/test_puzzle.py
@@ -4,7 +4,7 @@ import puzzle
 class TestBasic(unittest.TestCase):  
 
     def test_basic_parse(self):
-        data = puzzle.parse('''00100
+        data = puzzle.basicParse('''00100
         11110
         10110
         10111
@@ -16,18 +16,95 @@ class TestBasic(unittest.TestCase):
         11001
         00010
         01010'''.splitlines())
+        self.assertEqual([['0', '0', '1', '0', '0'], ['1', '1', '1', '1', '0'], ['1', '0', '1', '1', '0'], ['1', '0', '1', '1', '1'], ['1', '0', '1', '0', '1'], ['0', '1', '1', '1', '1'], ['0', '0', '1', '1', '1'], ['1', '1', '1', '0', '0'], ['1', '0', '0', '0', '0'], ['1', '1', '0', '0', '1'], ['0', '0', '0', '1', '0'], ['0', '1', '0', '1', '0']], data)  
+
+    def test_rotation(self):
+        input = puzzle.basicParse('''00100
+        11110
+        10110
+        10111
+        10101
+        01111
+        00111
+        11100
+        10000
+        11001
+        00010
+        01010'''.splitlines())
+        data = puzzle.rotateInput(input)
         self.assertEqual(['011110011100', '010001010101', '111111110000', '011101100011', '000111100100'], data)
 
     def test_basic_solve(self):
-        data = ['011110011100', '010001010101', '111111110000', '011101100011', '000111100100']
+        data = puzzle.basicParse('''00100
+        11110
+        10110
+        10111
+        10101
+        01111
+        00111
+        11100
+        10000
+        11001
+        00010
+        01010'''.splitlines())
         self.assertEqual(198, puzzle.solve(data))
 
     def test_puzzle_answer_part1(self): 
         input = open("03/input.txt", "r")
-        data = puzzle.parse(input.readlines()) 
-        answer = puzzle.solve(data)  
+        answer = puzzle.solve(input)  
         input.close()
         self.assertEqual(4138664, answer)
+
+    def test_solve2_oxygen(self):
+        input = '''00100
+        11110
+        10110
+        10111
+        10101
+        01111
+        00111
+        11100
+        10000
+        11001
+        00010
+        01010'''.splitlines()
+        self.assertEqual(23, puzzle.calculateOxygenRating(input))
+
+    def test_solve2_co2(self):
+        input = '''00100
+        11110
+        10110
+        10111
+        10101
+        01111
+        00111
+        11100
+        10000
+        11001
+        00010
+        01010'''.splitlines()
+        self.assertEqual(10, puzzle.calculateCO2Rating(input))
+
+    def test_solve2_full(self):
+        input = '''00100
+        11110
+        10110
+        10111
+        10101
+        01111
+        00111
+        11100
+        10000
+        11001
+        00010
+        01010'''.splitlines()
+        self.assertEqual(230, puzzle.solve2(input))
+
+    # def test_puzzle_answer_part1(self): 
+    #     input = open("03/input.txt", "r")
+    #     answer = puzzle.solve2(input)  
+    #     input.close()
+    #     self.assertEqual(0, answer)
 
 if __name__ == '__main__':  
     unittest.main()
