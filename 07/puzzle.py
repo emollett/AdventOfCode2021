ec76ce80687bacb7dc00cb88c570108acba497af
Eleanor Mollett <eleanor.mollett@gmail.com>
2021-12-07 10:01:59 +0000

    Day 7 part 2

--- 07/puzzle.py
+++ 07/puzzle.py
@@ -12,3 +12,17 @@ def solve(data):
 
 def calc_movement(crab_position, align_position):
     return abs(crab_position - align_position)
+
+def solve2(data):
+
+    fuel = []
+    i = 0
+    while i <= max(data):
+        fuel.append(sum(calc_movement2(crab_position, i) for crab_position in data))
+        i += 1
+
+    return min(fuel)
+
+def calc_movement2(crab_position, align_position):
+    return (1 + abs(crab_position - align_position)) * (abs(crab_position - align_position)/2)
+
--- 07/test_puzzle.py
+++ 07/test_puzzle.py
@@ -12,7 +12,21 @@ class TestBasic(unittest.TestCase):
         data = list(map(int, input.readline().split(",")))
         answer = puzzle.solve(data)  
         input.close()
-        self.assertEqual(342534, answer)   
+        self.assertEqual(342534, answer) 
+
+    def test_calc_crab_movement_2(self):
+        self.assertEqual(10, puzzle.calc_movement2(1, 5))
+
+    def test_basic_solve2(self):
+        data = [16,1,2,0,4,2,7,1,2,14]
+        self.assertEqual(168, puzzle.solve2(data))
+
+    def test_puzzle_answer_part2(self): 
+        input = open("07/input.txt", "r")
+        data = list(map(int, input.readline().split(",")))
+        answer = puzzle.solve2(data)  
+        input.close()
+        self.assertEqual(94004208, answer)   
 
 if __name__ == '__main__':  
     unittest.main()
