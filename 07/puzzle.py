b4f279cccc527ef15f474bcbe77556b07a71afb0
Eleanor Mollett <eleanor.mollett@gmail.com>
2021-12-07 09:35:55 +0000

    Day 7 part 1

--- 07/puzzle.py
+++ 07/puzzle.py
@@ -1,7 +1,14 @@
+import math
+
 def solve(data):
 
-    crab_position = list(map(int, data[0].split(",")))
+    fuel = []
+    i = 0
+    while i <= max(data):
+        fuel.append(sum(calc_movement(crab_position, i) for crab_position in data))
+        i += 1
 
-    #
+    return min(fuel)
 
-    return crab_position
+def calc_movement(crab_position, align_position):
+    return abs(crab_position - align_position)
--- 07/test_puzzle.py
+++ 07/test_puzzle.py
@@ -4,8 +4,15 @@ import puzzle
 class TestBasic(unittest.TestCase):  
 
     def test_basic_solve(self):
-        data = ['16,1,2,0,4,2,7,1,2,14']
-        self.assertEqual(4, puzzle.solve(data))
+        data = [16,1,2,0,4,2,7,1,2,14]
+        self.assertEqual(37, puzzle.solve(data))
+
+    def test_puzzle_answer_part1(self): 
+        input = open("07/input.txt", "r")
+        data = list(map(int, input.readline().split(",")))
+        answer = puzzle.solve(data)  
+        input.close()
+        self.assertEqual(342534, answer)   
 
 if __name__ == '__main__':  
     unittest.main()
