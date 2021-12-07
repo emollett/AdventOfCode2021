6c523c6e20fabfc88625e2bdf013483a7f225a6a
Eleanor Mollett <eleanor.mollett@gmail.com>
2021-12-07 08:37:31 +0000

    day 7 wip

---
+++ 07/puzzle.py
@@ -0,0 +1,7 @@
+def solve(data):
+
+    crab_position = list(map(int, data[0].split(",")))
+
+    #
+
+    return crab_position
--- 07/test_puzzle.py
+++ 07/test_puzzle.py
@@ -3,11 +3,9 @@ import puzzle
 
 class TestBasic(unittest.TestCase):  
 
-    def test_basic_parse(self):
-        data = puzzle.parse('''forward 5
-        down 5
-        forward 8'''.splitlines())
-        self.assertEqual([["forward", 5], ["down", 5], ["forward", 8]], data)
+    def test_basic_solve(self):
+        data = ['16,1,2,0,4,2,7,1,2,14']
+        self.assertEqual(4, puzzle.solve(data))
 
 if __name__ == '__main__':  
     unittest.main()
