7ca2452fd06e56addb52d33ae65aab1b7189dc26
Eleanor Mollett <eleanor.mollett@gmail.com>
2021-12-08 12:02:02 +0000

    day 8 wip

--- 08/puzzle.py
+++ 08/puzzle.py
@@ -19,3 +19,31 @@ def solve1(notes):
     for entry in notes:
         unique += countUnique(entry[1])
     return unique
+
+def identifyDigits(entry):
+    input = entry[0]
+    output = entry[1]
+    
+    one = any(len(digit) == 2 for digit in input)
+    four = any(len(digit) == 3 for digit in input)
+    seven = any(len(digit) == 4 for digit in input)
+    eight = any(len(digit) == 7 for digit in input)
+
+    for digit in input:
+        if len(digit) == 4:
+            if digit.split()
+
+    # 3 has both the letters 1 does and length 4
+    # 2 has 1 letter different from 3, which gives us e and f
+    # 5 has 1 letter different from 3, which gives us b and c, shares all but 1 with 4
+
+    #
+
+    # a is the difference between 1 and 7 freq 8
+    # b is the difference between 9 and 3 freq 6
+    # c is the difference between 5 and 9 freq 8
+    # d is the difference between 0 and 8 freq 7
+    # e is the difference between 5 and 6 freq 4
+    # f is the difference between ??????? freq 9
+    # g is the difference between ??????? freq 7
+
--- 08/test_puzzle.py
+++ 08/test_puzzle.py
@@ -11,18 +11,18 @@ class TestBasic(unittest.TestCase):
         data = ['acedgfb', 'cdfbe', 'gcdfa', 'fbcad', 'dab', 'cefabd', 'cdfgeb', 'eafb', 'cagedb', 'ab']
         self.assertEqual(4, puzzle.countUnique(data))
 
-    # def test_solve_part_1(self):
-    #     data= puzzle.parse('''be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
-    #     edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
-    #     fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
-    #     fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
-    #     aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
-    #     fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
-    #     dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
-    #     bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
-    #     egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
-    #     gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce'''.splitlines())
-    #     self.assertEqual(26, puzzle.solve1(data))
+    def test_solve_part_1(self):
+        data= puzzle.parse('''be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+        edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+        fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+        fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+        aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+        fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+        dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+        bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+        egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+        gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce'''.splitlines())
+        self.assertEqual(26, puzzle.solve1(data))
 
     def test_puzzle_answer_part1(self): 
         input = open("08/input.txt", "r")
@@ -31,19 +31,9 @@ class TestBasic(unittest.TestCase):
         input.close()
         self.assertEqual(330, answer) 
 
-    # def test_calc_crab_movement_2(self):
-    #     self.assertEqual(10, puzzle.calc_movement2(1, 5))
-
-    # def test_basic_solve2(self):
-    #     data = [16,1,2,0,4,2,7,1,2,14]
-    #     self.assertEqual(168, puzzle.solve(data, '2'))
-
-    # def test_puzzle_answer_part2(self): 
-    #     input = open("07/input.txt", "r")
-    #     data = list(map(int, input.readline().split(",")))
-    #     answer = puzzle.solve(data, '2')  
-    #     input.close()
-    #     self.assertEqual(94004208, answer)   
+    def test_identify_digits(self):
+        data = (['acedgfb', 'cdfbe', 'gcdfa', 'fbcad', 'dab', 'cefabd', 'cdfgeb', 'eafb', 'cagedb', 'ab'], ['cdfeb', 'fcadb', 'cdfeb', 'cdbaf'])
+        self.assertEqual(5353, puzzle.identifyDigits(data))
 
 if __name__ == '__main__':  
     unittest.main()
