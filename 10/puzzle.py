9c5b047bee5a1e4a6fc8a581719e8f14ad8f5154
Eleanor Mollett <eleanor.mollett@gmail.com>
2021-12-10 12:45:23 +0000

    day10 part 2

--- 10/puzzle.py
+++ 10/puzzle.py
@@ -1,8 +1,9 @@
 from typing import Counter
+import math
 
-def findCorruptingBracket(line):
+def processBrackets(line):
     opening_brackets = []
-    for index, bracket in enumerate(line):
+    for bracket in line:
         if bracket == '[' or bracket == '{' or bracket == '(' or bracket == '<':
             opening_brackets.append(bracket)
         elif bracket == ']' and opening_brackets[-1] == '[':
@@ -14,16 +15,37 @@ def findCorruptingBracket(line):
         elif bracket == '>' and opening_brackets[-1] == '<':
             opening_brackets.pop()
         else: return bracket
-    return 1
+    return opening_brackets
+
+def calculateAutocompleteScore(brackets):
+    score = 0
+    for bracket in reversed(brackets):
+        score = score * 5
+        if bracket == '(':
+            score += 1
+        if bracket == '[':
+            score += 2
+        if bracket == '{':
+            score += 3
+        if bracket == '<':
+            score += 4
+    return score
 
 def solve1(data):
     brackets = []
     for line in data:
-        brackets.append(findCorruptingBracket(line))
+        brackets.append(processBrackets(line))
+    brackets = [x for x in brackets if not isinstance(x, list)]
     bracket_count = Counter(brackets)
-
-#     ): 3 points.
-# ]: 57 points.
-# }: 1197 points.
-# >: 25137 points.
     return bracket_count[')']*3 + bracket_count[']']*57 + bracket_count['}']*1197 + bracket_count['>']*25137
+
+def solve2(data):
+    scores = []
+    for line in data:
+        brackets = processBrackets(line)
+        if isinstance(brackets, list):
+            score = calculateAutocompleteScore(brackets)
+            scores.append(score)
+    scores.sort()
+    middle_score = scores[math.floor(len(scores)/2)]
+    return middle_score
--- 10/test_puzzle.py
+++ 10/test_puzzle.py
@@ -1,3 +1,4 @@
+from datetime import date
 import unittest  
 import puzzle
 
@@ -5,7 +6,7 @@ class TestBasic(unittest.TestCase):
 
     def test_find_corrupting_character(self):
         data = '{([(<{}[<>[]}>{[]{[(<()>'
-        self.assertEqual('}', puzzle.findCorruptingBracket(data))
+        self.assertEqual('}', puzzle.processBrackets(data))
 
     def test_basic_solve(self):
         data = puzzle.solve1('''[({(<(())[]>[[{[]{<()<>>
@@ -25,7 +26,35 @@ class TestBasic(unittest.TestCase):
         data = input.read().splitlines()
         answer = puzzle.solve1(data)  
         input.close()
-        self.assertEqual(278475, answer)     
+        self.assertEqual(278475, answer)   
+
+    def test_find_unclosed_brackets(self):
+        data = '[({(<(())[]>[[{[]{<()<>>'
+        self.assertEqual(['[', '(', '{', '(', '[', '[', '{', '{'], puzzle.processBrackets(data))  
+
+    def test_calculate_autocomplete_score(self):
+        data = ['[', '(', '{', '(', '[', '[', '{', '{']
+        self.assertEqual(288957, puzzle.calculateAutocompleteScore(data))
+
+    def test_basic_solve2(self):
+        data = puzzle.solve2('''[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]'''.splitlines())
+        self.assertEqual(288957, data) 
+
+    def test_puzzle_answer_part2(self): 
+        input = open("10/input.txt", "r")
+        data = input.read().splitlines()
+        answer = puzzle.solve2(data)  
+        input.close()
+        self.assertEqual(3015539998, answer)  
 
 if __name__ == '__main__':  
     unittest.main()
