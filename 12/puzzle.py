f20653bd1f8a4fc0b5b7669a3d4f16d887483d4d
Eleanor Mollett <eleanor.mollett@gmail.com>
2021-12-13 17:26:20 +0000

    Day 12 part 2

--- 12/puzzle.py
+++ 12/puzzle.py
@@ -1,3 +1,5 @@
+from operator import itemgetter
+
 def parse(data):
     dots = []
     folds = []
@@ -14,22 +16,36 @@ def parse(data):
 
 def oneFold(coordinates, fold):
     direction, size = fold
-    new_coordinates = []
-    unchanged_coordinates = []
+
     if direction == 'y':
-        for coordinate in coordinates:
-            if coordinate[1] > size:
-                new_coordinate = (coordinate[0], size - (coordinate[1] - size))
-                new_coordinates.append(new_coordinate)
-            else:
-                unchanged_coordinates.append(coordinate)
+        new_coordinates = [(coordinate[0], size - (coordinate[1] - size)) for coordinate in coordinates if coordinate[1] > size]
+        unchanged_coordinates = [coordinate for coordinate in coordinates if not coordinate[1] > size]
     if direction == 'x':
-        for coordinate in coordinates:
-            if coordinate[0] > size:
-                new_coordinate = (size - (coordinate[0] - size), coordinate[1])
-                new_coordinates.append(new_coordinate)
-            else:
-                unchanged_coordinates.append(coordinate)
+        new_coordinates = [(size - (coordinate[0] - size), coordinate[1]) for coordinate in coordinates if coordinate[0] > size]
+        unchanged_coordinates = [coordinate for coordinate in coordinates if not coordinate[0] > size]
+
     new_coordinates.extend(unchanged_coordinates)
     folded_coordinates = list(dict.fromkeys(new_coordinates))
+
     return folded_coordinates
+
+def severalFolds(data):
+    dots, folds = parse(data)
+    for fold in folds:
+        dots = oneFold(dots, fold)
+    return dots
+
+def passwordPicture(data):
+    dots = severalFolds(data)
+    largest_x = max(dots, key=itemgetter(0))[0]
+    largest_y = max(dots, key=itemgetter(1))[0]
+    grid = []
+    for i in range(largest_y + 1):
+        grid.append(["  "] * (largest_x + 1))
+
+    for dot in dots:
+        grid[dot[1]][dot[0]] = '#'
+
+    for line in grid:
+        print(line)
+    return -1
--- 12/test_puzzle.py
+++ 12/test_puzzle.py
@@ -69,6 +69,59 @@ fold along x=55'''.splitlines())
         input.close()
         self.assertEqual(724, len(answer))  
 
+    def test_multiple_folds(self):
+        data = '''6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5'''.splitlines()
+        self.assertEqual(16, len(puzzle.severalFolds(data)))
+
+    def test_multiple_folds(self):
+        data = '''6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5'''.splitlines()
+        answer= puzzle.passwordPicture(data)
+
+    def test_puzzle_answer_part2(self): 
+        input = open("12/input.txt", "r")
+        data = input.read().splitlines()
+        answer = puzzle.passwordPicture(data)
+        input.close()
 
 if __name__ == '__main__':  
     unittest.main()
