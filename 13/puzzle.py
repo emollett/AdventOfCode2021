106cab067179fde1e7fc71efd461cb18895e81af
Eleanor Mollett <eleanor.mollett@gmail.com>
2021-12-17 22:05:51 +0000

    Day 13 part 2 WIP

--- 13/puzzle.py
+++ 13/puzzle.py
@@ -1,5 +1,6 @@
 from operator import itemgetter
 from typing import Counter
+import string
 
 def parse(data):
     template = data[0]
@@ -23,7 +24,57 @@ def multipleInsertions(data, rounds):
 
 def solve(data, rounds):
     template = multipleInsertions(data, rounds)
-    print(Counter(template).most_common(1)[0])
-    print(Counter(template).most_common()[-1])
     score = Counter(template).most_common(1)[0][1] - Counter(template).most_common()[-1][1]
     return score
+
+def onePair(pair, insertionMap):
+    insertion = insertionMap[pair]
+    new_pairs = [pair[0]+insertion, insertion+pair[1]]
+    return new_pairs
+
+def createInitialFrequencies(insertionMap):
+    pair_frequencies = {}
+    for pair in insertionMap:
+            pair_frequencies[pair] = 0
+    return pair_frequencies
+
+def updatePairFrequencies(pairs, pairFrequencies):
+    pairFrequencies = pairFrequencies.fromkeys(pairFrequencies, 0)
+    for pair in pairs:
+        newPair, multiplier = pair
+        pairFrequencies[newPair] += multiplier
+    return pairFrequencies
+
+def updateElementFrequencies(elements, elementFrequencies, multiplier):
+    for element in elements:
+        elementFrequencies[element] += multiplier
+    return elementFrequencies
+
+def createNewPairs(pairFrequencies, insertionMap, elementFrequencies):
+    pairsToUpdate = []
+    for pair, frequency in pairFrequencies.items():
+        if frequency > 0:
+            insertion = insertionMap[pair]
+            newPair1 = (pair[0]+insertion, frequency) 
+            newPair2 = (insertion+pair[1], frequency)
+            pairsToUpdate.extend([newPair1, newPair2])
+            elementFrequencies = updateElementFrequencies(insertion, elementFrequencies, frequency)
+    pairFrequencies = updatePairFrequencies(pairsToUpdate, pairFrequencies)
+    return pairFrequencies, elementFrequencies
+
+def solve2(data, rounds):
+    template, insertionMap = data
+    pairFrequencies = createInitialFrequencies(insertionMap)
+    elementFrequencies = dict.fromkeys(string.ascii_uppercase, 0)
+    elementFrequencies = updateElementFrequencies(template, elementFrequencies, 1)
+    initial_pairs = [(letter + template[i+1], 1) for i, letter in enumerate(template[:-1])]
+    pairFrequencies = updatePairFrequencies(initial_pairs, pairFrequencies)
+    for _ in range(rounds):
+        pairFrequencies, elementFrequencies = createNewPairs(pairFrequencies, insertionMap, elementFrequencies)
+    score = scoreElements(elementFrequencies)
+    return score
+
+def scoreElements(elementFrequencies):
+    score = max(elementFrequencies.values()) - min(filter(None, elementFrequencies.values()))
+    return score
+
--- 13/test_puzzle.py
+++ 13/test_puzzle.py
@@ -90,7 +90,7 @@ CN -> C'''.splitlines())
         answer = puzzle.multipleInsertions(data, 4)
         self.assertEqual('NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB', answer)
 
-    def test_solce1(self):
+    def test_solve1(self):
         data = puzzle.parse('''NNCB
 
 CH -> B
@@ -118,13 +118,130 @@ CN -> C'''.splitlines())
         answer = puzzle.solve(data, 10)
         input.close()
         self.assertEqual(2509, answer)  
+    
+    def test_one_pair(self):
+        pair = 'NN'
+        insertion_map = {'CH': 'B', 'HH': 'N', 'CB': 'H', 'NH': 'C', 'HB': 'C', 'HC': 'B', 'HN': 'C', 'NN': 'C', 'BH': 'H', 'NC': 'B', 'NB': 'B', 'BN': 'B', 'BB': 'N', 'BC': 'B', 'CC': 'N', 'CN': 'C'}
+        answer = puzzle.onePair(pair, insertion_map)
+        self.assertEqual(['NC', 'CN'], answer)
 
-    # def test_puzzle_answer_part1(self): 
-    #     input = open("13/input.txt", "r")
-    #     data = puzzle.parse(input.read().splitlines())
-    #     answer = puzzle.solve(data, 40)
-    #     input.close()
-    #     self.assertEqual(0, answer)  
+    def test_create_initial_frequencies(self):
+        insertion_map = {'CH': 'B', 'HH': 'N', 'CB': 'H', 'NH': 'C', 'HB': 'C', 'HC': 'B', 'HN': 'C', 'NN': 'C', 'BH': 'H', 'NC': 'B', 'NB': 'B', 'BN': 'B', 'BB': 'N', 'BC': 'B', 'CC': 'N', 'CN': 'C'}
+        answer = puzzle.createInitialFrequencies(insertion_map)
+        self.assertEqual({'CH': 0, 'HH': 0, 'CB': 0, 'NH': 0, 'HB': 0, 'HC': 0, 'HN': 0, 'NN': 0, 'BH': 0, 'NC': 0, 'NB': 0, 'BN': 0, 'BB': 0, 'BC': 0, 'CC': 0, 'CN': 0}, answer)
+
+    def test_update_frequencies(self):
+        pairs = [('NC', 2), ('CN', 2)]
+        frequencyMap = {'CH': 0, 'HH': 0, 'CB': 0, 'NH': 0, 'HB': 0, 'HC': 0, 'HN': 0, 'NN': 0, 'BH': 0, 'NC': 0, 'NB': 0, 'BN': 0, 'BB': 0, 'BC': 0, 'CC': 0, 'CN': 0}
+        answer = puzzle.updatePairFrequencies(pairs, frequencyMap)
+        self.assertEqual({'CH': 0, 'HH': 0, 'CB': 0, 'NH': 0, 'HB': 0, 'HC': 0, 'HN': 0, 'NN': 0, 'BH': 0, 'NC': 2, 'NB': 0, 'BN': 0, 'BB': 0, 'BC': 0, 'CC': 0, 'CN': 2}, answer)
+
+    def test_create_new_pairs(self):
+        frequencyMap = {'CH': 0, 'HH': 0, 'CB': 1, 'NH': 0, 'HB': 0, 'HC': 0, 'HN': 0, 'NN': 1, 'BH': 0, 'NC': 1, 'NB': 0, 'BN': 0, 'BB': 0, 'BC': 0, 'CC': 0, 'CN': 0}
+        insertion_map = {'CH': 'B', 'HH': 'N', 'CB': 'H', 'NH': 'C', 'HB': 'C', 'HC': 'B', 'HN': 'C', 'NN': 'C', 'BH': 'H', 'NC': 'B', 'NB': 'B', 'BN': 'B', 'BB': 'N', 'BC': 'B', 'CC': 'N', 'CN': 'C'}
+        elementFrequencies = {'A': 0, 'B': 1, 'C': 1, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 2, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}
+        answer = puzzle.createNewPairs(frequencyMap, insertion_map, elementFrequencies)
+        self.assertEqual({'CH': 1, 'HH': 0, 'CB': 0, 'NH': 0, 'HB': 1, 'HC': 0, 'HN': 0, 'NN': 0, 'BH': 0, 'NC': 1, 'NB': 1, 'BN': 0, 'BB': 0, 'BC': 1, 'CC': 0, 'CN': 1}, answer[0])
+
+    def test_score_elements(self):
+        elementFrequencies = {'A': 0, 'B': 0, 'C': 2, 'D': 10, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 4, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}
+        answer = puzzle.scoreElements(elementFrequencies)
+        self.assertEqual(8, answer)
+
+    def test_element_frequencies(self):
+        frequencyMap = {'CH': 0, 'HH': 0, 'CB': 1, 'NH': 0, 'HB': 0, 'HC': 0, 'HN': 0, 'NN': 1, 'BH': 0, 'NC': 1, 'NB': 0, 'BN': 0, 'BB': 0, 'BC': 0, 'CC': 0, 'CN': 0}
+        insertion_map = {'CH': 'B', 'HH': 'N', 'CB': 'H', 'NH': 'C', 'HB': 'C', 'HC': 'B', 'HN': 'C', 'NN': 'C', 'BH': 'H', 'NC': 'B', 'NB': 'B', 'BN': 'B', 'BB': 'N', 'BC': 'B', 'CC': 'N', 'CN': 'C'}
+        elementFrequencies = {'A': 0, 'B': 1, 'C': 1, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 2, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}
+        answer = puzzle.createNewPairs(frequencyMap, insertion_map, elementFrequencies)
+        self.assertEqual({'A': 0, 'B': 2, 'C': 2, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 1, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 2, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}, answer[1])
+
+    def test_element_frequencies_next(self):
+        frequencyMap = {'CH': 1, 'HH': 0, 'CB': 0, 'NH': 0, 'HB': 1, 'HC': 0, 'HN': 0, 'NN': 0, 'BH': 0, 'NC': 1, 'NB': 1, 'BN': 0, 'BB': 0, 'BC': 1, 'CC': 0, 'CN': 1}
+        insertion_map = {'CH': 'B', 'HH': 'N', 'CB': 'H', 'NH': 'C', 'HB': 'C', 'HC': 'B', 'HN': 'C', 'NN': 'C', 'BH': 'H', 'NC': 'B', 'NB': 'B', 'BN': 'B', 'BB': 'N', 'BC': 'B', 'CC': 'N', 'CN': 'C'}
+        elementFrequencies = {'A': 0, 'B': 2, 'C': 2, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 1, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 2, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}
+        answer = puzzle.createNewPairs(frequencyMap, insertion_map, elementFrequencies)
+        self.assertEqual({'A': 0, 'B': 6, 'C': 4, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 1, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 2, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}, answer[1])
+
+
+    def test_solve2(self):
+        data = puzzle.parse('''NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C'''.splitlines())
+        answer = puzzle.solve2(data, 2)
+        self.assertEqual(5, answer)  
+
+    def test_solve2_more(self):
+        data = puzzle.parse('''NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C'''.splitlines())
+        answer = puzzle.solve2(data, 3)
+        self.assertEqual(7, answer) 
+
+    def test_solve2_bigger(self):
+        data = puzzle.parse('''NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C'''.splitlines())
+        answer = puzzle.solve2(data, 10)
+        self.assertEqual(1588, answer)   
+
+    def test_puzzle_answer_part1_with_solve2(self): 
+        input = open("13/input.txt", "r")
+        data = puzzle.parse(input.read().splitlines())
+        answer = puzzle.solve2(data, 10)
+        input.close()
+        self.assertEqual(2509, answer)       
+
+    def test_puzzle_answer_part1(self): 
+        input = open("13/input.txt", "r")
+        data = puzzle.parse(input.read().splitlines())
+        answer = puzzle.solve(data, 40)
+        input.close()
+        self.assertEqual(0, answer)  
 
 
 if __name__ == '__main__':  
